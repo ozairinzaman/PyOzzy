346928148d2342c50a34150fcd2438b92e25854e
Ozair Inzaman <20230240@student.unifiji.ac.fj>
2024-03-10 19:12:33 +1200

    Onyx

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        SecondClass Msg = new SecondClass();
+        Msg.Message();
+    }
+}
+
+class SecondClass
+{
+    public void Message(){
+        System.out.println("Hello World, I am Ozair Inzaman");
+    }
+}
